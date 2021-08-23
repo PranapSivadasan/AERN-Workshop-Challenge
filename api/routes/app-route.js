bcc1fc00c31cbaf55b1c50b43e8bdef1b435181a
PranapSivadasan <pranapsivadasan99@gmail.com>
2021-08-23 23:22:48 +0530

    API - Added ratings API and created common service

--- api/routes/app-route.js
+++ api/routes/app-route.js
@@ -1,9 +1,11 @@
 const bookRouter = require('./book-route');
 const categoryRouter = require('./category-route');
+const ratingRouter = require('./rating-route');
 
 const router = require('express').Router();
 
 router.use('/api/books', bookRouter);
 router.use('/api/categories', categoryRouter);
+router.use('/api/ratings', ratingRouter);
 
 module.exports = router;
