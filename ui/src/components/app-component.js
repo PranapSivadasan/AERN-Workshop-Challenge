01cad8af845d6e1f294b1ccaf3fbb1d3cd631742
PranapSivadasan <pranapsivadasan99@gmail.com>
2021-08-28 00:59:10 +0530

    UI - Added Login and register page

--- ui/src/components/app-component.js
+++ ui/src/components/app-component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Switch, Route } from 'react-router-dom';
 import BookComponent from './books/book-component';
@@ -8,39 +8,51 @@ import DashboardComponent from './dashboard-component';
 
 import FooterComponent from "./footer-component";
 import HeaderComponent from "./header-component";
+import LoginPage from './login/login-page';
 import NavbarComponent from './navbar-component';
 
 const AppComponent = () => {
 
 
-    return (
-        <div className="page-grid">
-            {/* <HeaderComponent></HeaderComponent> */}
-            <NavbarComponent></NavbarComponent>
-            <div className="container-fluid">
-                {/* <Container> */}
-                <Switch>
-                    <Route exact path='/'>
-                        <DashboardComponent></DashboardComponent>
-                    </Route>
-                    <Route exact path='/books'>
-                        <BookComponent></BookComponent>
-                        {/* <Row>
-                            <Col>
-                                <BookListComponent openDetails={(val) => {showBookDetails(val)}}></BookListComponent>
-                            </Col>
-                            <Col sm={8} style={{display: showDetails? '':'none'}}>
-                                <BookDetailsComponent></BookDetailsComponent>
-                            </Col>
-                        </Row> */}
-                    </Route>
-                    {/* <Route exact path='/books/:bookId'> */}
-                    {/* </Route> */}
-                </Switch>
-                {/* </Container> */}
-            </div>
-            <FooterComponent></FooterComponent>
+    const [loginSuccess, updateLoginStatus] = useState(false);
+
+    let render;
+    const loginPage = <LoginPage changeLogin = {(val) => updateLoginStatus(val)} />;
+    const mainPage = <div className="page-grid">
+        {/* <HeaderComponent></HeaderComponent> */}
+        <NavbarComponent></NavbarComponent>
+        <div className="container-fluid">
+            {/* <Container> */}
+            <Switch>
+                <Route exact path='/dashboard'>
+                    <DashboardComponent></DashboardComponent>
+                </Route>
+                <Route exact path='/books'>
+                    <BookComponent></BookComponent>
+                    {/* <Row>
+                    <Col>
+                        <BookListComponent openDetails={(val) => {showBookDetails(val)}}></BookListComponent>
+                    </Col>
+                    <Col sm={8} style={{display: showDetails? '':'none'}}>
+                        <BookDetailsComponent></BookDetailsComponent>
+                    </Col>
+                </Row> */}
+                </Route>
+                {/* <Route exact path='/books/:bookId'> */}
+                {/* </Route> */}
+            </Switch>
+            {/* </Container> */}
         </div>
+        <FooterComponent></FooterComponent>
+    </div>;
+
+    if (loginSuccess) {
+        render = mainPage;
+    } else {
+        render = loginPage;
+    }
+    return (    
+        render
     );
 }
 
