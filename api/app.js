9cca7cd74c336a555ad8d56ba52a29606e0b9345
PranapSivadasan <pranapsivadasan99@gmail.com>
2021-08-29 20:58:48 +0530

    API - Moinor changes

--- api/app.js
+++ api/app.js
@@ -11,6 +11,7 @@ app.listen(process.env.PORT, () => {
 // Adding CORS policy
 app.use( (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", 
         "Origin, X-Requested-With, Content-Type, Accept");
     next();
